0a958376e4da2d92a93ad3cb3ed5e74a9ab876e7
Track404 <111713000+Track404@users.noreply.github.com>
2023-11-22 16:09:21 +0100

    finish the display of who is the winner After the game it now show you who won

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// function that randomly choosing between rock paper and scissors 
+
 function getComputerChoice() {
     all_choice = ["rock","paper","scissors"];
     index =Math.floor(Math.random()*all_choice.length);
@@ -6,6 +8,7 @@ function getComputerChoice() {
     return ChoiceComputer;
 }
 
+// fucntion that play a round of rock,paper,scissors
 
 function PlayRound(player_choice, computerChoice) {
     
@@ -14,38 +17,60 @@ function PlayRound(player_choice, computerChoice) {
     if (player_choice == "rock" && computerChoice =="scissors"||
         player_choice == "paper" && computerChoice =="rock"||
         player_choice == "scissors" && computerChoice =="paper"){
-    result =`You Win! ${player_choice} beats ${computerChoice}`;
-    player++;
+    result= "player";
+    
     }
     else if(player_choice == computerChoice){
-        result= "it's a draw";
+        result= "draw";
     }
     else { 
-        result= `You Lose! ${player_choice} beats ${computerChoice}`;
-        computer++;
+        result= "computer";
+        
     }
 return result
 }
+
 let player=0;
 let computer=0;
-let numberOfRound =0;
+let numberOfRound =1;
 let tablescore =[player, computer];
 let playerSelection = "rock";
 
+//loop to play a game of 5 rounds of rock,paper,scissors
 
-while(numberOfRound<5){
+while(numberOfRound<6){
     let computerSelection = getComputerChoice();
-    console.log(computerSelection);
-    console.log(PlayRound(playerSelection,computerSelection));
+    PlayRound(playerSelection,computerSelection);
+    console.log("----------------");
+    console.log(`Round number ${numberOfRound} !`);
+    if(result=="player"){
+        console.log("You win")
+        tablescore[0]++;
+    }
+    else if(result=="computer"){
+        console.log("You lose")
+        tablescore[1]++;
+    }
+    else{
+        console.log("it's a draw")
+    }
+    
     numberOfRound+=1;
     
 }
- if(tablescore[0]>tablescore[1]){
-    console.log("player win");
+//display result of the game
+
+console.log("              ")
+console.log("Result of the game:");
+
+// if condition to check who is the winner between the player and the computer and display it
+
+if(tablescore[0]>tablescore[1]){
+    console.log("Player win the game !");
  }
  else if( tablescore[0]<tablescore[1]){
-    console.log("computer win");
+    console.log("Computer win the game !");
  }
  else{
-    console.log("its a draw");
+    console.log("its a draw try again !");
  }
