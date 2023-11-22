ac32d5127df48236795f37aa8739ee41093d2605
Track404 <111713000+Track404@users.noreply.github.com>
2023-11-22 15:25:44 +0100

    création de la boucle whille pour faire une partie de 5 round

--- script.js
+++ script.js
@@ -8,35 +8,44 @@ function getComputerChoice() {
 
 
 function PlayRound(player_choice, computerChoice) {
-    let player =0;
+    
+    let player=0;
     let computer=0;
-    
-    if (player_choice == "rock" && computerChoice =="scissors"){
-    result ="You Win! Rock beats Scissors";
-    
-    }
-    else if(player_choice == "paper" && computerChoice =="rock"){
-        result ="You Win! Paper beats Rock";
-
-    }
-    else if(player_choice == "scissors" && computerChoice =="paper"){
-        result ="You Win! Scissors beats Paper";
+    if (player_choice == "rock" && computerChoice =="scissors"||
+        player_choice == "paper" && computerChoice =="rock"||
+        player_choice == "scissors" && computerChoice =="paper"){
+    result =`You Win! ${player_choice} beats ${computerChoice}`;
+    player++;
     }
     else if(player_choice == computerChoice){
         result= "it's a draw";
     }
     else { 
-        result= `You Lose! ${player_choice} beats ${computerChoice}`}
+        result= `You Lose! ${player_choice} beats ${computerChoice}`;
+        computer++;
+    }
 return result
 }
+let player=0;
+let computer=0;
+let numberOfRound =0;
+let tablescore =[player, computer];
+let playerSelection = "rock";
 
 
-let i=0;
-let playerSelection = "rock"
-let computerSelection = getComputerChoice()
-console.log(playerSelection)
-console.log(computerSelection);
-
-
-console.log(PlayRound(playerSelection,computerSelection));
-
+while(numberOfRound<5){
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    console.log(PlayRound(playerSelection,computerSelection));
+    numberOfRound+=1;
+    
+}
+ if(tablescore[0]>tablescore[1]){
+    console.log("player win");
+ }
+ else if( tablescore[0]<tablescore[1]){
+    console.log("computer win");
+ }
+ else{
+    console.log("its a draw");
+ }
